50a5c0d4576aafe8f60beeae1de1fa1e14d54afc
Runeblaze <facss_carpool_service@unc.edu>
2019-04-13 16:50:35 -0400

    added real programs

---
+++ adder_iso.java
@@ -0,0 +1,9 @@
+class Adder {
+    static add(int x, int y) {
+        return x + y;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(add(5, 7));
+    }
+}
---
+++ arith_iso.java
@@ -0,0 +1,6 @@
+class A {
+    public static void main(String[] args) {
+        int x = 3 + 5 + 7 + 9 - 3 * 5 - 99 * 22 / 3;
+        System.out.println(x);
+    }
+}
